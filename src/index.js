c17915cfb6586baf41f04bdbad0081242174927c
Xavier Loup <loupxavi@amazon.com>
2019-08-21 10:13:23 +0000

    New React and Bootstrap application

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+  This CodeSandbox has been automatically generated using
+  `codesandboxer`. If you're curious how that happened, you can
+  check out our docs here: https://github.com/noviny/codesandboxer
+
+  If you experience any struggles with this sandbox, please raise an issue
+  on github. :)
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+);
