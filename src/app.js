06a86167875a7bba886d6f8c930bb46d8ac8b881
Xavier Loup <loupxavi@amazon.com>
2019-08-23 13:31:46 +0000

    Thank Clemence

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ function About() {
               A simple chatbot project to demonstrate a multi-lingual serverless chatbot running on the AWS platform.
             </p>
             <p>
-              Project developed by Xavier Loup with the help of its colleagues
+              Project developed by Xavier Loup with the help of its colleagues. Chatbot image by Clémence.  
             </p>
           </Col>
           <Col lg="4">
